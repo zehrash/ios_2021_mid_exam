8fe3095bf236d5e09d6e49b775f6cbd903a8dcf3
Zehra Iliyaz <zehrash11@gmail.com>
2022-01-07 17:09:33 +0200

    Initial Commit

---
+++ ios_2021_mid_exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_2021_mid_exam
+//
+//  Created by ZehraIliyaz  on 7.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
