a994e83b69a4cd2990a8d9b56c6280e5db6bb7d9
Zehra Iliyaz <zehrash11@gmail.com>
2022-01-07 17:13:02 +0200

    initial commit

--- ios_2021_mid_exam/ViewController.swift
+++ ios_2021_mid_exam/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// brace yourself, exam is coming
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
